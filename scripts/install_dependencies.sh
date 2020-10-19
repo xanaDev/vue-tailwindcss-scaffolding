5d16684762c47213b70b531bb0d6ace4a2d55bf3
abid elhaj <xanaghost@gmail.com>
2020-10-19 16:40:45 +0100

    fix: update installation phase

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,12 +1,29 @@
 #!/usr/bin/env bash
 
-if ! command -v node &> /dev/null
-then
-    echo "node could not be found, installing ..."
-    sudo amazon-linux-extras install epel
-    sudo yum install nodejs -y
-    sudo npm install -g yarn
-    exit
-fi
+install_service() {
+  serviceName=$1
 
-sudo yum install httpd -y
+  if systemctl --all --type service | grep -q "$serviceName";then
+      echo "$serviceName exists."
+  else
+      echo "$serviceName does NOT exist."
+      sudo yum install httpd -y
+  fi
+}
+
+install_process() {
+  process=$1
+  if ! command -v $process &> /dev/null
+  then
+      echo "node could not be found, installing ..."
+      sudo amazon-linux-extras install epel
+      sudo yum install nodejs -y
+      sudo npm install -g yarn
+      exit
+  fi
+}
+
+
+install_service "httpd"
+
+install_process "node"
